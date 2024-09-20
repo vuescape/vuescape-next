7f6341357b607d1cdde17d40b61ab070cde966be
randylevy <randy.ley@gmail.com>
2024-09-20 05:07:20 -0400

    Add 404 page

---
+++ src/components/404Page.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <h3>Page Not Found</h3>
+  <div>
+    <br />
+    We're sorry, but the page you requested cannot be found. Please check the website address and try again or try
+    navigating with the menu.
+    Clicking the CoMetrics Logo at the top left will take you to the home page.
+  </div>
+</template>
