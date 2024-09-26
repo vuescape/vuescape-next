42a4a675cf5bc4d7303d37223c83cb4fbb52bab3
randylevy <randy.ley@gmail.com>
2024-09-26 00:12:54 -0400

    Update config to use nx tasks

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
+import { resolve } from 'path'
+
 import { defineConfig, mergeConfig } from 'vite'
+
 import rootConfig from '../../vite.config'
 
-import path, { resolve } from 'path'
-
-const localConfig =  defineConfig({
+const localConfig = defineConfig({
   // plugins: [vue(), dts({ rollupTypes: true })],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-    },
+      '@': resolve(__dirname, 'src')
+    }
   },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'vuescape',
-      fileName: 'vuescape',
-    },
-  },
+      fileName: 'vuescape'
+    }
+  }
 })
 
-export default mergeConfig(rootConfig, localConfig);
+export default mergeConfig(rootConfig, localConfig)
